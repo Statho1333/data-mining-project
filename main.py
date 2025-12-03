89a23aeba5dcf85e37de9084d4e3a53660df15e1
Statho1333 <istathop@csd.auth.gr>
2025-12-03 07:58:27 +0200

    same commit, internet crashed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 from pathlib import Path 
-
+import matplotlib.pyplot as plt
 
 
 
@@ -21,13 +21,25 @@ def parser():
     else:
         return file_path
 
+#Plots the dataframe given as input
+def plotDframe(df):
+    plt.scatter(df[0], df[1], alpha=0.5, marker='o')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.savefig('scatter_plot.png')
+    #plt.show()
 
-
+def dfPreprocess(df):
+    pass
 
 def main():
-    ds = parser()
-    ds = pd.read_csv(ds)
-    print(ds)
+    ds_name = parser()
+    df = pd.read_csv(ds_name, on_bad_lines='skip', header=None)
+    df = df.apply(pd.to_numeric, errors='coerce').fillna(0.0)
+    print(df.dtypes)
+    print(df.columns)
+    plotDframe(df)
+    print(df)
     
 
 if __name__ == "__main__":
